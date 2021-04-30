ffa602c264c35eb8ca63921af4033204b0d65986
SweetyKandekar <81276279+SweetyKandekar@users.noreply.github.com>
2021-04-30 13:55:02 +0530

    Update CucumberConfig.ts

--- CucumberConfig.ts
+++ CucumberConfig.ts
@@ -15,7 +15,7 @@ export let config: Config = {
     
    capabilities: {
      browserName: 'firefox',
-     'args': ['--safe-mode'],
+    // 'args': ['--safe-mode'],
      'moz:firefoxOptions': {
       args: [ "--headless" ]
   },
